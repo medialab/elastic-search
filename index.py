09a70b874ac9324b109199384bd0e3403b9870cc
Kelly Christensen <kelly.christensen@sciencespo.fr>
2022-10-13 15:51:01 +0200

    add progress bar

--- index.py
+++ index.py
@@ -4,12 +4,15 @@ import os
 
 import click
 from elasticsearch import Elasticsearch
+from tqdm.auto import tqdm
 
 
 @click.command()
 @click.argument("index")
 def main(index):
-# CONFIGURATION FOR ELASTIC SEARCH CLIENT
+
+    # ----------------------------------------------------#
+    # CONFIGURATION FOR ELASTIC SEARCH CLIENT
     PERSONAL_CONFIG = "config.json"
     EXAMPLE_CONFIG = "example.config.json"
 
@@ -23,6 +26,7 @@ def main(index):
         CERT_FINGERPRINT = config["CERT_FINGERPRINT"]
         DATA = os.path.join("data", config["DATA_FILE"])
 
+    # ----------------------------------------------------#
     # CREATE CLIENT
     client = Elasticsearch(
         "https://localhost:9200",
@@ -30,18 +34,31 @@ def main(index):
         basic_auth=("elastic", ELASTIC_PASSWORD)
         )
 
+    # ----------------------------------------------------#
+    # CREATE INDEX WITH MAPPING
     with open("db_mappings.json", "r") as f:
         mapping = json.load(f)
 
-    # CREATE INDEX WITH MAPPING
     if not client.indices.exists(index=index):
         client.indices.create(index=index, **mapping)
 
+    # ----------------------------------------------------#
     # ADD DOCUMENTS
     with open(DATA, "r") as f:
         csv_reader = csv.DictReader(f)
+
+        total = len(list(csv_reader))
+        f.seek(0)
+        csv_reader.__next__()
+
+        pbar = tqdm(total=total, desc="Progress Bar", dynamic_ncols=True)
+
         for row in csv_reader:
             client.index(index=index, id=row.pop("unique_id"), document=row)
+            pbar.update(len(row)/10)
+
+        pbar.close()
+
 
 if __name__ == "__main__":
     main()
